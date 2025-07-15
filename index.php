eb4d28818b1987f0e844151ccafe61ed5d57403f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-07-15 10:15:49 +0700

    Test running php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Project PHP Pertamaku</title>
+</head>
+
+<body>
+    <h1>Halo, Dunia PHP!</h1>
+
+    <?php
+    echo "Waktu sekarang: " . date("Y-m-d H:i:s");
+    ?>
+</body>
+
+</html>
